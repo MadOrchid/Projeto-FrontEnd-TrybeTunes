1969c4f05c9453c692c5f6005b3244fb95efae21
Leonardo Begnossi <leo.agb_@hotmail.com>
2022-03-07 15:21:55 -0300

    requisito 2 finalizado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,71 @@
 import React from 'react';
 import { Redirect } from 'react-router';
-import { creatUser } from '../services/userAPI';
+import { createUser } from '../services/userAPI';
 import Loading from './Loading';
 
 class Login extends React.Component {
   constructor() {
     super();
-    this.state
-}
+    this.state = {
+      name: '',
+      buttonStatus: true,
+      loading: false,
+      loaded: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(type) {
+    const typeText = type.target.value;
+    const timeLegth = 3;
+
+    this.setState({ name: typeText });
+    return (typeText.length >= timeLegth)
+      ? this.setState({ buttonStatus: false })
+      : this.setState({ buttonStatus: true });
+  }
+
+  handleClick(click) {
+    click.preventDefault();
+    this.setState({ loading: true });
+    const { name } = this.state;
+    createUser({ name }).then(() => this.setState({ loaded: true, loading: false }));
+  }
 
   render() {
-    return (
+    const {
+      buttonStatus,
+      loading,
+      loaded,
+    } = this.state;
+    const load = (
       <div data-testid="page-login">
-        <h1>Login</h1>
+        <form>
+          <label htmlFor="name-input">
+            <input
+              type="text"
+              name="name-input"
+              data-testid="login-name-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            onClick={ this.handleClick }
+            disabled={ buttonStatus }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
+    );
+    return (
+      <>
+        { loading ? <Loading /> : load }
+        { loaded && <Redirect to="/search" />}
+      </>
     );
   }
 }
