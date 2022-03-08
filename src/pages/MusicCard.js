dc34f0af94cca1895dd27627548233b9c1325ab9
Leonardo Begnossi <leo.agb_@hotmail.com>
2022-03-08 17:59:18 -0300

    requisito 7

---
+++ src/pages/MusicCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends React.Component {
+  render() {
+    const { song } = this.props;
+    return (
+      <>
+        <h1 key={ song.trackName }>{ song.trackName }</h1>
+        <audio data-testid="audio-component" src={ song.previewUrl } constrols>
+          <track kind="captions" />
+        </audio>
+      </>
+    );
+  }
+}
+MusicCard.propTypes = {
+  song: PropTypes.shape().isRequired,
+};
+
+export default MusicCard;
